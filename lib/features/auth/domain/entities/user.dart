2ab59878fff6fb0865518459cd4023f7f511c6f3
Vladyslav ALISOV <vladyslav.alisov@gmail.com>
2025-04-12 14:42:33 +0300

    removes firebase

--- lib/features/auth/domain/entities/user.dart
+++
@@ -1,9 +0,0 @@
-class AuthAppUser {
-  final String userId;
-  final String email;
-
-  AuthAppUser({
-    required this.userId,
-    this.email = "",
-  });
-}
--- lib/features/navigation_screen/presentation/screens/navigation_screen.dart
+++ lib/features/navigation_screen/presentation/screens/navigation_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_template/core/router/app_routes.dart';
 import 'package:flutter_bloc_template/features/navigation_screen/presentation/navigation_cubit/navigation_cubit.dart';
-import 'package:flutter_bloc_template/init_dependencies.dart' show serviceLocator;
-import 'package:go_router/go_router.dart';
 
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
@@ -25,33 +22,26 @@ class _NavigationScreenState extends State<NavigationScreen> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
-      child: BlocListener<AuthBloc, AuthState>(
-        listener: (context, state) {
-          if (state is AuthInitial) {
-            context.go(AppRoutes.signUp.path);
-          }
+      child: BlocBuilder<NavigationCubit, NavigationState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: IndexedStack(
+              index: state.index,
+              children: _navBarScreens,
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (value) => context.read<NavigationCubit>().navigate(
+                    NavigationScreenRoute.values[value],
+                  ),
+              currentIndex: state.index,
+              items: [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: "Programs"),
+                BottomNavigationBarItem(icon: Icon(Icons.school), label: "My Applications"),
+                BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+              ],
+            ),
+          );
         },
-        child: BlocBuilder<NavigationCubit, NavigationState>(
-          builder: (context, state) {
-            return Scaffold(
-              body: IndexedStack(
-                index: state.index,
-                children: _navBarScreens,
-              ),
-              bottomNavigationBar: BottomNavigationBar(
-                onTap: (value) => context.read<NavigationCubit>().navigate(
-                      NavigationScreenRoute.values[value],
-                    ),
-                currentIndex: state.index,
-                items: [
-                  BottomNavigationBarItem(icon: Icon(Icons.home), label: "Programs"),
-                  BottomNavigationBarItem(icon: Icon(Icons.school), label: "My Applications"),
-                  BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-                ],
-              ),
-            );
-          },
-        ),
       ),
     );
   }
